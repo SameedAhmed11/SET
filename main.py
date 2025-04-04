7ee1cb346deb5d42b89efc1d5411b0d25180b956
SameedAhmed11 <139317577+SameedAhmed11@users.noreply.github.com>
2025-04-04 12:57:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from openai import OpenAI
+import time
+import streamlit as st
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="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",
+)
+
+@st.cache_data
+def analyze_feedback(feedback_text):
+    completion = client.chat.completions.create(
+        model="deepseek/deepseek-r1:free",
+        messages=[
+            {"role": "system", "content": "Classify the following feedback as 'Positive', 'Negative', or 'Neutral'. Only return one word: 'Positive', 'Negative', or 'Neutral'.  Respond with only one word per feedback in the same order. If the feedback contains mixed sentiments, classify it according to the dominant sentiment."},
+            {"role": "user", "content": feedback_text},
+        ]
+    )
+    return completion.choices[0].message.content.strip()
+
+
+st.markdown("# Input text to Test Model :robot_face:")
+feedback = st.text_input("",placeholder="Enter text here...")
+
+st.write("Sentiment: ",analyze_feedback(feedback))
+
+
